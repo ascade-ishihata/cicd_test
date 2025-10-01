867d1f5b44224f046bd42466d644d4e8d7de1a31
ascade-ishihata <ishihata@ascade.co.jp>
2025-10-02 01:18:48 +0900

    fix8

--- hoge2.sh
+++ hoge2.sh
@@ -6,21 +6,24 @@ DEST="/home/ec2-user/cicd_test"
 LOG="${DEST}/hoge2.log"
 
 CP="/usr/bin/cp"
-CD="/usr/bin/cd"
 DATE="/usr/bin/date"
 PWD_CMD="/usr/bin/pwd"
 LS="/usr/bin/ls"
 GIT="/usr/bin/git"
+PKILL="/usr/bin/pkill"
 UV="${HOME}/.local/bin/uv"
 NOHUP="/usr/bin/nohup"
 
 ${CP} /dev/null ${LOG}
-#${CD} ${DEST}
+
+# Use the built-in cd instead of /usr/bin/cd.
 cd ${DEST}/
+
 ${DATE} >> ${LOG}
 echo "pwd: `${PWD_CMD}`" >> ${LOG}
 ${LS} >> ${LOG}
 ${GIT} config --global --add safe.directory ${DEST} >> ${LOG}
 
+${PKILL} python >> ${LOG} 2>&1
 ${UV} sync >> ${LOG} 2>&1
 ${NOHUP} ${UV} run main.py > nohup.log 2>&1 &
